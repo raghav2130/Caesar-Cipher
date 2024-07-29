b7a34bffbc0484f20b4e8317126a454b6bb0a41c
Raghav Goel <93156524+raghav2130@users.noreply.github.com>
2024-07-29 23:26:09 +0530

    first upload of cipher

---
+++ main.py
@@ -0,0 +1,36 @@
+data = input("Enter the Data to encrypt: ")
+shift = int(input("Enter shift number (+ve or -ve): "))
+
+if shift == 0:
+    print("cipher_text: " + data)
+
+# alpha = "abcdefghijklmnopqrstuvwxyz"
+
+cipher_text = ""
+for c in data:
+    ascii = ord(c)
+    # print(ascii)
+    ascii = ascii + shift
+    if 'a' <= c <= 'z':
+        # our range is 97 to 122 inclusive
+        if ascii > 122:
+            ascii = 96 + (ascii-122)
+        elif ascii < 97:
+            ascii = (ascii-97) + 123
+        # print(chr(ascii))
+        cipher_text += chr(ascii)
+    elif 'A' <= c <= 'Z':
+        # our range is 65 to 90 inclusive
+        if ascii > 90:
+            ascii = 64 + (ascii-90)
+        elif ascii < 65:
+            ascii = (ascii-65) + 91
+        # print(chr(ascii))
+        cipher_text += chr(ascii)
+    else:
+        cipher_text += c
+
+
+print("cipher_text after shift: " + cipher_text)
+
+    
